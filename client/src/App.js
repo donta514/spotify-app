2b1deff28ac11f63f07ee55c65eeefa07480cabc
donta514 <dnovsmith.ds20@gmail.com>
2023-09-06 14:25:03 -0400

    build out profile page with api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { GlobalStyle } from "./styles";
-import { Login } from "./pages";
+import { Login, Profile } from "./pages";
 import styled from "styled-components/macro";
 import { useEffect, useState } from "react";
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
@@ -10,6 +10,22 @@ import {
   Route,
   useLocation,
 } from "react-router-dom";
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
 
 // Scroll to top of page when changing routes
 // https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
@@ -45,33 +61,19 @@ function App() {
         {!token ? (
           <Login />
         ) : (
-          <Router>
-            <ScrollToTop />
-            <Routes>
-              <Route path="/top-artists" element={<h1>Top Artists</h1>} />
-              <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
-              <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
-              <Route path="/playlists" element={<h1>Playlists</h1>} />
-              <Route
-                path="/"
-                element={
-                  <>
-                    <button onClick={logout}>Log Out</button>
-
-                    {profile && (
-                      <div>
-                        <h1>{profile.display_name}</h1>
-                        <p>{profile.followers.total} Followers</p>
-                        {profile.images.length && profile.images[0].url && (
-                          <img src={profile.images[0].url} alt="Avatar" />
-                        )}
-                      </div>
-                    )}
-                  </>
-                }
-              />
-            </Routes>
-          </Router>
+          <>
+            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+            <Router>
+              <ScrollToTop />
+              <Routes>
+                <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+                <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+                <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+                <Route path="/playlists" element={<h1>Playlists</h1>} />
+                <Route path="/" element={<Profile />} />
+              </Routes>
+            </Router>
+          </>
         )}
       </header>
     </div>
