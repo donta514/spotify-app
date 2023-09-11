8b2ca7fbe5a5164b72be8ce958159dba30018890
donta514 <dnovsmith.ds20@gmail.com>
2023-09-11 14:02:31 -0400

    heroku configuration

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -42,14 +42,14 @@ const StyledLoginButton = styled.a`
 //   }
 // `;
 
+const LOGIN_URI =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:8888/login"
+    : "https://spotify-profile-v2.herokuapp.com/login";
+
 const Login = () => (
   <StyledLoginContainer>
-    <StyledLoginButton href="http://localhost:8888/login">
-      Log in to Spotify
-    </StyledLoginButton>
-    {/* <StyledAppleButton href="http://localhost:8888/login">
-      Log in to Apple Music
-    </StyledAppleButton> */}
+    <StyledLoginButton href={LOGIN_URI}>Log in to Spotify</StyledLoginButton>
   </StyledLoginContainer>
 );
 
--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const axios = require("axios");
 const querystring = require("querystring");
 const app = express();
-const port = 8888;
-
+const path = require("path");
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
+const PORT = process.env.PORT || 8888;
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 /**
  * Generates a random string containing numbers and letters
@@ -77,7 +81,7 @@ app.get("/callback", (req, res) => {
           expires_in,
         });
 
-        res.redirect(`http://localhost:3000/?${queryParams}`);
+        res.redirect(`${FRONTEND_URI}?${queryParams}`);
       } else {
         res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
       }
@@ -112,6 +116,11 @@ app.get("/refresh_token", (req, res) => {
     });
 });
 
-app.listen(port, () => {
-  console.log(`Express backend listening on port ${port}`);
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Express backend listening on port ${PORT}`);
 });
